1f47f7108670f621c923a0d67b62918f9544782a
Apurba <ap_nat@yahoo.com>
2014-08-04 21:10:52 +0530

    wip version of source lookup

--- components/shell-enhancements/SourceUtil.scala
+++ components/shell-enhancements/SourceUtil.scala
@@ -3,7 +3,8 @@ import org.objectweb.asm.util._
 import java.io._
 
 object SourceUtil {
-  def classLoader = SourceUtil.getClass.getClassLoader.asInstanceOf[scala.tools.nsc.interpreter.IMain$TranslatingClassLoader]
+  def classLoader = SourceUtil.getClass.getClassLoader.asInstanceOf[org.apache.spark.repl.SparkIMain$TranslatingClassLoader]
+  val indexes: collection.mutable.Map[String, String]  = collection.mutable.Map()
 
   def getClassBytes(className:String):Array[Byte] = {
     val bytes:Array[Byte] = classLoader.classBytes(className)
@@ -15,8 +16,12 @@ object SourceUtil {
     cReader.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0)
   }
 
-  def source(className:String) = {
+  def printType(obj:Any) = {
+    print(obj.getClass.getName)
+  }
 
+  def source(className:String) = {
+    printSourceForPath(indexes(className))
   }
 
   def printSelf() = {
@@ -32,4 +37,22 @@ object SourceUtil {
       stream.close();
     }
   }
+
+  def indexFolder(path:String) = {
+    indexes ++= accumulateFiles(new File(path))
+  }
+
+  private def printSourceForPath(filePath:String) = {
+    val source = scala.io.Source.fromFile(filePath)
+    source.getLines.foreach{
+         println
+    }
+    source.close
+  }
+
+  private def accumulateFiles(f: File): Map[String,String] = {
+    val these = f.listFiles
+    these.filter(_.isDirectory == false ).foldLeft(Map():Map[String,String])((r,file) => if (file.getName.endsWith("scala") || file.getName.endsWith("java")) { r ++ Map(file.getName-> file.getAbsolutePath)} else r) ++ these.filter(_.isDirectory).foldLeft(Map():Map[String,String])((r,file) => r ++ accumulateFiles(file))
+  }
+    
 }
